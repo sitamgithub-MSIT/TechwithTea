e298c7e6ed07c5f69727f1b127ba8f61c33e15df
Sitam <sitammeur@gmail.com>
2023-07-30 05:10:57 +0530

    added the search feature

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from blog import views
 
 
 urlpatterns = [
+    path('search/', views.search, name='search'),
     path("<slug:category_slug>/<slug:slug>/", views.blogpageview, name="blogpageview"),
     path("<slug:slug>/", views.categorypageview, name="categorypageview"),
 ]
