db716b9a79f25f679143297fcb9a8a2f26eeff5e
Sitam <sitammeur@gmail.com>
2023-12-06 13:10:47 +0530

    docs added comment form

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,17 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for creating a comment.
+
+    Inherits from forms.ModelForm and defines the fields for the Comment model.
+
+    Attributes:
+        model (Comment): The Comment model to use for the form.
+        fields (tuple): The fields to include in the form.
+
+    """
+
     class Meta:
         model = Comment
         fields = ("name", "email", "body")
