4d4decafba3b2b746f83b97dfc376a8c34e8368a
Sitam <sitammeur@gmail.com>
2023-12-06 13:33:30 +0530

    url file docs added

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,15 @@ from blog import views
 
 
 urlpatterns = [
-    path('search/', views.search, name='search'),
+    path("search/", views.search, name="search"),
     path("<slug:category_slug>/<slug:slug>/", views.blogpageview, name="blogpageview"),
     path("<slug:slug>/", views.categorypageview, name="categorypageview"),
 ]
+
+"""
+URL patterns for the blog application.
+
+- The 'search/' path is used to handle search functionality.
+- The '<slug:category_slug>/<slug:slug>/' path is used to display a specific blog post.
+- The '<slug:slug>/' path is used to display blog posts belonging to a specific category.
+"""
