6a487484f55844ad226a61e348c62c337e8ef5a9
Sitam <sitammeur@gmail.com>
2023-12-06 13:21:02 +0530

    app congif docs added

--- blog/apps.py
+++ blog/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+    """
+    Configuration class for the 'blog' app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "blog"
