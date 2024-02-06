627643c70bcacfbe0dc6ea19a3c93a5f7ab92640
Sitam <sitammeur@gmail.com>
2024-02-07 03:34:06 +0530

    docs modified for urls file

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
 from blog import views
 
-
-urlpatterns = [
-    path("search/", views.search, name="search"),
-    path("<slug:category_slug>/<slug:slug>/", views.blogpageview, name="blogpageview"),
-    path("<slug:slug>/", views.categorypageview, name="categorypageview"),
-]
-
 """
 URL patterns for the blog application.
 
@@ -15,3 +8,9 @@ URL patterns for the blog application.
 - The '<slug:category_slug>/<slug:slug>/' path is used to display a specific blog post.
 - The '<slug:slug>/' path is used to display blog posts belonging to a specific category.
 """
+
+urlpatterns = [
+    path("search/", views.search, name="search"),
+    path("<slug:category_slug>/<slug:slug>/", views.blogpageview, name="blogpageview"),
+    path("<slug:slug>/", views.categorypageview, name="categorypageview"),
+]
