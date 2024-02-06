08da60baa0207747c5a1df805a6596b1c0b60b8c
Sitam <sitammeur@gmail.com>
2024-02-07 03:41:35 +0530

    comments added to the views file

--- blog/views.py
+++ blog/views.py
@@ -20,9 +20,11 @@ def blogpageview(request, category_slug, slug):
     """
     post = get_object_or_404(Post, slug=slug, status=Post.ACTIVE)
 
+    # If the request is a POST request, process the comment form
     if request.method == "POST":
         form = CommentForm(request.POST)
 
+        # If the form is valid, save the comment and redirect to the blog post
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
@@ -33,6 +35,7 @@ def blogpageview(request, category_slug, slug):
     else:
         form = CommentForm()
 
+    # Render the blog post and comment form
     return render(request, "blog/blogdetails.html", {"post": post, "form": form})
 
 
@@ -47,9 +50,12 @@ def categorypageview(request, slug):
     Returns:
         HttpResponse: The rendered category details page.
     """
+
+    # Get the category object and all the posts belonging to the category
     category = get_object_or_404(Category, slug=slug)
     posts = category.posts.filter(status=Post.ACTIVE)
 
+    # Render the category details page
     return render(
         request, "blog/categorydetails.html", {"category": category, "posts": posts}
     )
@@ -65,8 +71,14 @@ def search(request):
     Returns:
         HttpResponse: The HTTP response object containing the search results.
     """
+
+    # Get the search query from the request
     query = request.GET.get("query")
+
+    # Get the posts that match the search query
     posts = Post.objects.filter(status=Post.ACTIVE).filter(
         Q(title__icontains=query) | Q(body__icontains=query) | Q(intro__icontains=query)
     )
+
+    # Render the search results page
     return render(request, "blog/search.html", {"posts": posts, "query": query})
